dc223891fc7927c354659ed8dc0cf40a5a960ee5
Asterisk-Light <aegislightvt@proton.me>
2026-03-12 13:25:13 +0000

    first commit

---
+++ screenFlask.py
@@ -0,0 +1,66 @@
+# Do this thing first
+# pip install flask aiortc opencv-python mss av
+
+import asyncio
+import json
+import cv2
+import numpy as np
+import mss
+
+from flask import Flask, request, jsonify, render_template
+from aiortc import RTCPeerConnection, RTCSessionDescription, VideoStreamTrack
+from av import VideoFrame
+
+app = Flask(__name__)
+
+pcs = set()
+
+class ScreenTrack(VideoStreamTrack):
+
+    def __init__(self):
+        super().__init__()
+        self.sct = mss.mss()
+        self.monitor = self.sct.monitors[1]
+
+    async def recv(self):
+        pts, time_base = await self.next_timestamp()
+
+        img = np.array(self.sct.grab(self.monitor))
+        img = cv2.cvtColor(img, cv2.COLOR_BGRA2BGR)
+
+        frame = VideoFrame.from_ndarray(img, format="bgr24")
+        frame.pts = pts
+        frame.time_base = time_base
+
+        return frame
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/offer", methods=["POST"])
+async def offer():
+
+    params = request.json
+    offer = RTCSessionDescription(sdp=params["sdp"], type=params["type"])
+
+    pc = RTCPeerConnection()
+    pcs.add(pc)
+
+    pc.addTrack(ScreenTrack())
+
+    await pc.setRemoteDescription(offer)
+
+    answer = await pc.createAnswer()
+    await pc.setLocalDescription(answer)
+
+    return jsonify({
+        "sdp": pc.localDescription.sdp,
+        "type": pc.localDescription.type
+    })
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
