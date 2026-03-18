fbe48acadbf04ab0fe614e6d2ac5103e85fde550
Asterisk-Light <aegislightvt@proton.me>
2026-03-18 13:10:56 +0000

    new basis

--- game.py
+++ game.py
@@ -1,4 +1,15 @@
 # code for game
 
-import gpiozero
+from gpiozero import Button
 import os
+
+p1left = Button()
+p1right = Button()
+p1attack = Button()
+p1block = Button()
+
+
+p2left = Button()
+p2right = Button()
+p2attack = Button()
+p2block = Button()
