73cd3ad076d5be1cc9760a05dbf0fa33077952fd
Asterisk-Light <aegislightvt@proton.me>
2026-03-18 14:07:08 +0000

    pygames library added + dictionary for gpio

--- game.py
+++ game.py
@@ -2,16 +2,37 @@
 
 from gpiozero import Button
 import os
-# import pygames at some point
+import pygame
 
-p1left = Button()
-p1right = Button()
-p1attack = Button()
-p1block = Button()
+p1left = Button(23)
+p1right = Button(24)
+p1attack = Button(25)
+p1block = Button(12)
 
 
-p2left = Button()
-p2right = Button()
-p2attack = Button()
-p2block = Button()
+p2left = Button(17)
+p2right = Button(27)
+p2attack = Button(22)
+p2block = Button(5)
+
+
+P1_LEFT = pygame.K_a
+P1_RIGHT = pygame.K_d
+P1_ATTACK = pygame.K_w
+P1_BLOCK = pygame.K_e
+P2_LEFT = pygame.K_LEFT
+P2_RIGHT = pygame.K_RIGHT
+P2_ATTACK = pygame.K_o
+P2_BLOCK = pygame.K_p
+
+gpioToPygameKey = {
+    p1left: P1_LEFT,
+    p1right: P1_RIGHT,
+    p1attack: P1_ATTACK,
+    p1block: P1_BLOCK,
+    p2left: P2_LEFT,
+    p2right: P2_RIGHT,
+    p2attack: P2_ATTACK,
+    p2block: P2_BLOCK
+}
 
